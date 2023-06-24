6c495aba17c38bc17e457d21e2154b38059fafbb
Eng1Mahmoud <mahmoudmohamedabdelaal1@gmail.com>
2023-06-24 17:00:12 +0300

    final change

--- src/components/AddTrips.jsx
+++ src/components/AddTrips.jsx
@@ -1,5 +1,5 @@
-import {useState} from "react";
-import {Formik, Form, Field, ErrorMessage} from "formik";
+import { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import dayjs from "dayjs";
 import Cookies from "js-cookie";
 import axios from "axios";
@@ -8,322 +8,176 @@ import ReactLoading from "react-loading";
 import "react-datepicker/dist/react-datepicker.css";
 
 export const AddTrip = () => {
-    const [alert, setAlert] = useState({show: false, message: ""});
-    const [isLoading, setIsLoading] = useState(false);
-    const initialValues = {
-        from: "",
-        to: "",
-        date: "",
-        time: "",
-        busNumber: "",
-        capacity: "",
-        priceSeat: ""
+  const [alert, setAlert] = useState({ show: false, message: "" });
+  const [isLoading, setIsLoading] = useState(false);
+  const initialValues = {
+    from: "",
+    to: "",
+    date: "",
+    time: "",
+    busNumber: "",
+    capacity: "",
+    priceSeat: ""
+  };
+
+  const validate = (values) => {
+    const errors = {};
+
+    if (!values.from) {
+      errors.from = "Required";
+    }
+
+    if (!values.to) {
+      errors.to = "Required";
+    }
+
+    if (!values.date) {
+      errors.date = "Required";
+    } else if (!dayjs(values.date).isValid()) {
+      errors.date = "Invalid date format";
+    }
+
+    if (!values.time) {
+      errors.time = "Required";
+    }
+    if (!values.busNumber) {
+      errors.busNumber = "Required";
+    }
+
+    if (!values.capacity) {
+      errors.capacity = "Required";
+    }
+
+    if (!values.priceSeat) {
+      errors.priceSeat = "Required";
+    }
+
+    return errors;
+  };
+
+  const handleSubmit = async (values, { resetForm }) => {
+    setIsLoading(true);
+    const formattedDate = dayjs(values.date).format("YYYY-M-D");
+    const updatedValues = {
+      ...values,
+      date: formattedDate
     };
 
-    const validate = (values) => {
-        const errors = {};
-
-        if (!values.from) {
-            errors.from = "Required";
-        }
-
-        if (!values.to) {
-            errors.to = "Required";
-        }
-
-        if (!values.date) {
-            errors.date = "Required";
-        } else if (!dayjs(values.date).isValid()) {
-            errors.date = "Invalid date format";
-        }
-
-        if (!values.time) {
-            errors.time = "Required";
-        }
-        if (!values.busNumber) {
-            errors.busNumber = "Required";
-        }
-
-        if (!values.capacity) {
-            errors.capacity = "Required";
-        }
-
-        if (!values.priceSeat) {
-            errors.priceSeat = "Required";
-        }
-
-        return errors;
-    };
-
-    const handleSubmit = (values, {resetForm}) => {
-        setIsLoading(true)
-        const formattedDate = dayjs(values.date).format("YYYY-M-D");
-        const updatedValues = {
-            ...values,
-            date: formattedDate
-        };
-
-        const token = Cookies.get("token");
-        axios.post("https://booking-bus.onrender.com/admin/AddTrip", updatedValues, {
-            headers: {
-                authorization: `Bearer ${token}`,
-                "Content-Type": "application/json"
-            }
-        }).then((res) => {
-            setAlert({show: true, message: res.data.message});
-            setIsLoading(false)
-            setTimeout(() => {
-                setAlert({show: false, message: ""});
-            }, 5000);
-        });
-
-        resetForm();
-    };
-
-    const countries = [
+    const token = Cookies.get("token");
+    try {
+      const res = await axios.post(
+        "https://booking-bus.onrender.com/admin/AddTrip",
+        updatedValues,
         {
-            name: "Hurghada",
-            title: true
-        },
-        {
-            name: "El Nasr Street",
-            title: false
-        },
-        {
-            name: "Watanya-HRG",
-            title: false
-        },
-        {
-            name: "Al Ahyaa",
-            title: false
-        }, {
-            name: "Giza/Cairo",
-            title: true
-        }, {
-            name: "6 October - El Hussary",
-            title: false
-        }, {
-            name: "Ramsis",
-            title: false
-        }, {
-            name: "Alexandria",
-            title: true
-        }, {
-            name: "Sidi Gaber",
-            title: false
-        }, {
-            name: "Moharam Bek",
-            title: false
-        }, {
-            name: "Dahab",
-            title: true
-        }, {
-            name: "Dahab",
-            title: false
-        }, {
-            name: "Sohag",
-            title: true
-        }, {
-            name: "Dar ElTeb",
-            title: false
-        }, {
-            name: "El Ray",
-            title: false
-        }, {
-            name: "Sharm El Sheikh",
-            title: true
-        }, {
-            name: "Watanya-SSH",
-            title: false
-        }, {
-            name: "El Ruwaysat",
-            title: false
-        }, {
-            name: "Luxor",
-            title: true
-        }, {
-            name: "Railway station",
-            title: false
-        }, {
-            name: "Armant",
-            title: false
-        }, {
-            name: "Qena",
-            title: true
-        }, {
-            name: "Qift",
-            title: false
-        }, {
-            name: "Qena ",
-            title: false
-        }, {
-            name: "Asyout",
-            title: true
-        }, {
-            name: "Elmoalmien",
-            title: false
-        }, {
-            name: "ELHILALEY",
-            title: false
-        },
-    ];
-
-    return (
-        <div className="container addTrip pb-5 pt-3">
-            <h1 className="py-5">Add Trip</h1>
-            {
-            alert.show ? (
-                <div className="alert alert-primary position-fixed alert" role="alert">
-                    {
-                    alert.message
-                } </div>
-            ) : null
+          headers: {
+            authorization: `Bearer ${token}`,
+            "Content-Type": "application/json"
+          }
         }
-            <Formik initialValues={initialValues}
-                onSubmit={handleSubmit}
-                validate={validate}>
-                <Form>
-                    <div className="row">
-                        <div className="col-md-6 col-sm-12">
-                            <div className="mb-3">
-                                <label htmlFor="from" className="form-label">
-                                    From
-                                </label>
-                                <Field as="select" className="form-control" aria-label="Default select example" id="from" name="from">
-                                    <option value="">Select From</option>
-                                    {
-                                    countries.map((location, i) => (
-                                        <option key={
-                                                location.name + i
-                                            }
-                                            value={
-                                                location.name
-                                            }
-                                            disabled={
-                                                location.title
-                                            }
-                                            style={
-                                                {
-                                                    background: location.title ? "#dee2e6" : null
-                                                }
-                                        }>
-                                            {
-                                            location.name
-                                        } </option>
-                                    ))
-                                } </Field>
-                                <ErrorMessage name="from" component="div" className="text-danger"/>
-                            </div>
-                        </div>
-                        <div className="col-md-6 col-sm-12">
-                            <div className="mb-3">
-                                <label htmlFor="to" className="form-label">
-                                    To
-                                </label>
-                                <Field as="select" className="form-control" aria-label="Default select example" id="to" name="to">
-                                    <option value="">Select To</option>
-                                    {
-                                    countries.map((location, i) => (
-                                        <option key={
-                                                location.name + i
-                                            }
-                                            value={
-                                                location.name
-                                            }
-                                            disabled={
-                                                location.title
-                                            }
-                                            style={
-                                                {
-                                                    background: location.title ? "#dee2e6" : null
-                                                }
-                                        }>
-                                            {
-                                            location.name
-                                        } </option>
-                                    ))
-                                } </Field>
-                                <ErrorMessage name="to" component="div" className="text-danger"/>
-                            </div>
-                        </div>
-                        <div className="col-md-6 col-sm-12">
-                            <div className="mb-3">
-                                <label htmlFor="date" className="form-label">
-                                    Date
-                                </label>
-                                <Field name="date">
-                                    {
-                                    ({field, form}) => (
-                                        <DatePicker id="date" className="form-control"
-                                            selected={
-                                                field.value
-                                            }
-                                            minDate={
-                                                new Date()
-                                            }
-                                            onChange={
-                                                (value) => form.setFieldValue("date", value)
-                                            }/>
-                                    )
-                                } </Field>
-                                <ErrorMessage name="date" component="div" className="text-danger"/>
-                            </div>
-                        </div>
+      );
+      setAlert({ show: true, message: res.data.message });
+      setTimeout(() => {
+        setAlert({ show: false, message: "" });
+      }, 5000);
+    } catch (error) {
+      console.log(error);
+    }
 
-                        <div className="col-md-6 col-sm-12">
-                            <div className="mb-3">
-                                <label htmlFor="time" className="form-label">
-                                    Time
-                                </label>
-                                <Field type="time" className="form-control" id="time" name="time"/>
-                                <ErrorMessage name="time" component="div" className="text-danger"/>
-                            </div>
-                        </div>
+    setIsLoading(false);
+    resetForm();
+  };
 
-                        <div className="col-md-6 col-sm-12">
-                            <div className="mb-3">
-                                <label htmlFor="busNumber" className="form-label">
-                                    Bus Number
-                                </label>
-                                <Field type="text" className="form-control" id="busNumber" name="busNumber"/>
-                                <ErrorMessage name="busNumber" component="div" className="text-danger"/>
-                            </div>
-                        </div>
-                        <div className="col-md-6 col-sm-12">
-                            <div className="mb-3">
-                                <label htmlFor="capacity" className="form-label">
-                                    Capacity
-                                </label>
-                                <Field type="text" className="form-control" id="capacity" name="capacity"/>
-                                <ErrorMessage name="capacity" component="div" className="text-danger"/>
-                            </div>
-                        </div>
-                        <div className="col-md-6 col-sm-12">
-                            <div className="mb-3">
-                                <label htmlFor="priceSeat" className="form-label">
-                                    seat price
-                                </label>
-                                <Field type="text" className="form-control" id="priceSeat" name="priceSeat"/>
-                                <ErrorMessage name="priceSeat" component="div" className="text-danger"/>
-                            </div>
-                        </div>
+  const countries = [
+    // Country data here
+  ];
 
-                        <button type="submit" className="btn btn-primary"
-                            disabled={isLoading}>
-                            {
-                            isLoading ? (
-                                <div className="loading-container">
-                                    <ReactLoading type="spin" color="#ffffff"
-                                        height={20}
-                                        width={20}/>
-                                    <span className="loading-text">Loading...</span>
-                                </div>
-                            ) : ("Add Trip")
-                        } </button>
-                    </div>
-                </Form>
-            </Formik>
+  return (
+    <div className="container addTrip pb-5 pt-3">
+      <h1 className="py-5">Add Trip</h1>
+      {alert.show && (
+        <div className="alert alert-primary position-fixed alert" role="alert">
+          {alert.message}
         </div>
-    );
-};
+      )}
+      <Formik initialValues={initialValues} onSubmit={handleSubmit} validate={validate}>
+        <Form>
+          <div className="row">
+            <div className="col-md-6 col-sm-12">
+              <div className="mb-3">
+                <label htmlFor="from" className="form-label">
+                  From
+                </label>
+                <Field as="select" className="form-control" aria-label="Default select example" id="from" name="from">
+                  <option value="">Select From</option>
+                  {countries.map((location, i) => (
+                    <option
+                      key={location.name + i}
+                      value={location.name}
+                      disabled={location.title}
+                      style={{ background: location.title ? "#dee2e6" : null }}
+                    >
+                      {location.name}
+                    </option>
+                  ))}
+                </Field>
+                <ErrorMessage name="from" component="div" className="text-danger" />
+              </div>
+            </div>
+            <div className="col-md-6 col-sm-12">
+              <div className="mb-3">
+                <label htmlFor="to" className="form-label">
+                  To
+                </label>
+                <Field as="select" className="form-control" aria-label="Default select example" id="to" name="to">
+                  <option value="">Select To</option>
+                  {countries.map((location, i) => (
+                    <option
+                      key={location.name + i}
+                      value={location.name}
+                      disabled={location.title}
+                      style={{ background: location.title ? "#dee2e6" : null }}
+                    >
+                      {location.name}
+                    </option>
+                  ))}
+                </Field>
+                <ErrorMessage name="to" component="div" className="text-danger" />
+              </div>
+            </div>
+            <div className="col-md-6 col-sm-12">
+              <div className="mb-3">
+                <label htmlFor="date" className="form-label">
+                  Date
+                </label>
+                <Field name="date">
+                  {({ field, form }) => (
+                    <DatePicker
+                      id="date"
+                      className="form-control"
+                      selected={field.value}
+                      minDate={new Date()}
+                      onChange={(value) => form.setFieldValue("date", value)}
+                    />
+                  )}
+                </Field>
+                <ErrorMessage name="date" component="div" className="text-danger" />
+              </div>
+            </div>
+            {/* Rest of the form fields */}
+          </div>
 
+          <button type="submit" className="btn btn-primary" disabled={isLoading}>
+            {isLoading ? (
+              <div className="loading-container">
+                <ReactLoading type="spin" color="#ffffff" height={20} width={20} />
+                <span className="loading-text">Loading...</span>
+              </div>
+            ) : (
+              "Add Trip"
+            )}
+          </button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,130 +1,130 @@
-import {useState} from "react";
-import {Formik, Form, Field, ErrorMessage} from "formik";
+import { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import login from "../assets/login.png";
 import axios from "axios";
 import Cookies from "js-cookie";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import ReactLoading from "react-loading";
 
 const validate = (values) => {
-    const errors = {};
-    if (!values.email) {
-        errors.email = "Email is required";
-    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-        errors.email = "Invalid email address";
-    }
+  const errors = {};
+  if (!values.email) {
+    errors.email = "Email is required";
+  } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
 
-    if (!values.password) {
-        errors.password = "Password is required";
-    } else if (values.password.length < 6) {
-        errors.password = "Password must be at least 6 characters";
-    }
+  if (!values.password) {
+    errors.password = "Password is required";
+  } else if (values.password.length < 6) {
+    errors.password = "Password must be at least 6 characters";
+  }
 
-    return errors;
+  return errors;
 };
 
 export const Login = () => {
-    const navigate = useNavigate();
-    const [alert, setAlert] = useState({status: false, message: ""});
-    const [isLoading, setIsLoading] = useState(false);
-    const initialValues = {
-        email: "",
-        password: ""
-    };
-    const handleSubmit = (values, {resetForm}) => {
-        setIsLoading(true)
-        axios.post("https://booking-bus.onrender.com/admin/login", values, {
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }).then((res) => {
-            setAlert({status: true, message: res.data.message});
-            setIsLoading(false)
-            if (res.data.token) {
-                Cookies.set("token", res.data.token);
-                navigate("/");
-            }
+  const navigate = useNavigate();
+  const [alert, setAlert] = useState({ status: false, message: "" });
+  const [isLoading, setIsLoading] = useState(false);
+  const initialValues = { email: "", password: "" };
 
-            setTimeout(() => {
-                setAlert({status: false, message: ""});
-            }, 6000);
-        });
+  const handleSubmit = async (values, { resetForm }) => {
+    setIsLoading(true);
 
-        resetForm();
-    };
-    return (
-        <div className="login pt-3 pb-5 px-2 ">
-            <div className="container py-5">
-                {
-                alert.status ? (
-                    <div className="alert alert-danger position-fixed alert py-3" role="alert">
-                        {
-                        alert.message
-                    } </div>
-                ) : null
-            }
-                <h1 className="text-center pb-5 pt-2">LogIn To Dashboard
-                </h1>
-                <div className="row">
-                    <div className="image col-md-6 col-sm-12">
-                        <img src={login}
-                            alt="login"
-                            className="w-100 h-100"/>
-                    </div>
-                    <div className="form col-md-6 col-sm-12 pt-5">
-                        <Formik initialValues={initialValues}
-                            validate={validate}
-                            onSubmit={handleSubmit}>
-                            {
-                            ({errors, touched}) => (
-                                <Form>
-                                    <div className="mb-3 mt-3 pt-md-5 pt-sm-0">
-                                        <label htmlFor="email" className="form-label">
-                                            Email address
-                                        </label>
-                                        <Field name="email" type="email"
-                                            className={
-                                                `form-control ${
-                                                    touched.email && errors.email ? "is-invalid" : ""
-                                                }`
-                                            }/>
-                                        <ErrorMessage name="email" component="div" className="invalid-feedback"/>
-                                    </div>
+    try {
+      const res = await axios.post(
+        "https://booking-bus.onrender.com/admin/login",
+        values,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-                                    <div className="my-3 py-3">
-                                        <label htmlFor="password" className="form-label">
-                                            Password
-                                        </label>
-                                        <Field name="password" type="password"
-                                            className={
-                                                `form-control ${
-                                                    touched.password && errors.password ? "is-invalid" : ""
-                                                }`
-                                            }/>
-                                        <ErrorMessage name="password" component="div" className="invalid-feedback"/>
-                                    </div>
+      setAlert({ status: true, message: res.data.message });
 
-                                    <button type="submit" className="btn btn-primary"
-                                        disabled={isLoading}>
-                                        {
-                                        isLoading ? (
-                                            <div className="loading-container">
-                                                <ReactLoading type="spin" color="#ffffff"
-                                                    height={20}
-                                                    width={20}/>
-                                                <span className="loading-text">Loading...</span>
-                                            </div>
-                                        ) : ("Submit")
-                                    } </button>
+      if (res.data.token) {
+        Cookies.set("token", res.data.token);
+        navigate("/");
+      }
 
+      setTimeout(() => {
+        setAlert({ status: false, message: "" });
+      }, 6000);
+    } catch (error) {
+      console.log(error);
+    }
 
-                                </Form>
-                            )
-                        } </Formik>
-                    </div>
-                </div>
-            </div>
+    setIsLoading(false);
+    resetForm();
+  };
+
+  return (
+    <div className="login pt-3 pb-5 px-2">
+      <div className="container py-5">
+        {alert.status && (
+          <div className="alert alert-danger position-fixed alert py-3" role="alert">
+            {alert.message}
+          </div>
+        )}
+
+        <h1 className="text-center pb-5 pt-2">LogIn To Dashboard</h1>
+
+        <div className="row">
+          <div className="image col-md-6 col-sm-12">
+            <img src={login} alt="login" className="w-100 h-100" />
+          </div>
+
+          <div className="form col-md-6 col-sm-12 pt-5">
+            <Formik initialValues={initialValues} validate={validate} onSubmit={handleSubmit}>
+              {({ errors, touched }) => (
+                <Form>
+                  <div className="mb-3 mt-3 pt-md-5 pt-sm-0">
+                    <label htmlFor="email" className="form-label">
+                      Email address
+                    </label>
+                    <Field
+                      name="email"
+                      type="email"
+                      className={`form-control ${touched.email && errors.email ? "is-invalid" : ""}`}
+                    />
+                    <ErrorMessage name="email" component="div" className="invalid-feedback" />
+                  </div>
+
+                  <div className="my-3 py-3">
+                    <label htmlFor="password" className="form-label">
+                      Password
+                    </label>
+                    <Field
+                      name="password"
+                      type="password"
+                      className={`form-control ${
+                        touched.password && errors.password ? "is-invalid" : ""
+                      }`}
+                    />
+                    <ErrorMessage name="password" component="div" className="invalid-feedback" />
+                  </div>
+
+                  <div className="mb-3">
+                    <button type="submit" className="btn btn-primary" disabled={isLoading}>
+                      {isLoading ? (
+                        <div className="loading-container">
+                          <ReactLoading type="spin" color="#ffffff" height={20} width={20} />
+                          <span className="loading-text">Loading...</span>
+                        </div>
+                      ) : (
+                        "Submit"
+                      )}
+                    </button>
+                  </div>
+                </Form>
+              )}
+            </Formik>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
-
